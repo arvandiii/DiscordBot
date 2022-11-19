f43c24d85850bcaf450dce1443d761171bc7fe60
Alireza Arvandi <arvandi.a@gmail.com>
2022-11-19 21:35:55 +0000

    post request

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser');
+
 const port = 3000
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
+app.post('/init', function(req, res) {
+    console.log('receiving data ...');
+    console.log('body is ',req.body);
+    res.send(req.body);
+});
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })

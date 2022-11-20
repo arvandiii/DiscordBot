4aa665b7ae8a39d62f8357141b08aa3b06f89b41
Alireza Arvandi <arvandi.a@gmail.com>
2022-11-20 06:46:06 +0000

    finalize

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -5,6 +5,7 @@ module.exports = {
 		.setName('ping')
 		.setDescription('Replies with Pong!'),
 	async execute(interaction) {
+		console.log(interaction.guild)
 		await interaction.reply('Pong!');
 	},
 };
--- src/commands/team-registration.js
+++ src/commands/team-registration.js
@@ -1,5 +1,6 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, PermissionsBitField } = require('discord.js');
 const { Group } = require('../models/Group');
+
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -27,12 +28,27 @@ module.exports = {
         if (teammate2) {
             members.push(teammate2.id)
         }
-        console.log('aaaaaa', members)
         await interaction.reply(
             `:white_check_mark:Team Registered\nTeam Name: ${teamName}
 Members: ${interaction.user.toString()}, ${teammate.toString()}${teammate2 ? ' ,' + teammate2.toString() : ''}`)
         await Group.findOneAndUpdate({ serverId: interaction.guildId }, { $set: { teamName, members: [] } }, { new: true, upsert: true })
-        const groups = await Group.find({})
-        console.log('saadfadsfas', groups)
+        const teamChannel = await interaction.guild.channels.cache.find(channel => channel.name == "Team");
+        const everyoneRole = interaction.guild.roles.everyone;
+        const overwritePermissions = members.map(m =>
+            ({ type: 'member', id: m, allow: [PermissionsBitField.Flags.ViewChannel] }))
+        overwritePermissions.push({ type: 'role', id: everyoneRole.id, deny: [PermissionsBitField.Flags.ViewChannel] })
+        console.log('per per', overwritePermissions)
+        const textChannel = await interaction.guild.channels.create({
+            name: `team-${teamName}`,
+            type: 0,
+            permissionOverwrites: overwritePermissions
+        })
+        await textChannel.setParent(teamChannel.id,  { lockPermissions: false })
+        const voiceChannel = await interaction.guild.channels.create({
+            name: `team-${teamName}`,
+            type: 2,
+            permissionOverwrites: overwritePermissions
+        })
+        await voiceChannel.setParent(teamChannel.id,  { lockPermissions: false })
     },
 }
